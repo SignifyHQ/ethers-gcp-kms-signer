591d69405f4fac6c467319c8e4d5fc883599c420
lily.hu <lily@57blocks.com>
2024-04-24 14:09:02 +0800

    add test siger for testing

---
+++ src/scripts/test-signsignTransaction.ts
@@ -0,0 +1,127 @@
+import configs from "dotenv";
+import { JsonRpcProvider, Contract } from "ethers";
+import { GcpKmsSignerCredentials, TestGcpKmsSigner } from "../signer-test";
+import BN from "bn.js";
+
+configs.config();
+
+const provider = new JsonRpcProvider("http://localhost:8545");
+
+const kmsCredentials: GcpKmsSignerCredentials = {
+  projectId: process.env.KMS_PROJECT_ID,
+  locationId: process.env.KMS_LOCATION_ID,
+  keyRingId: process.env.KMS_KEY_RING_ID,
+  keyId: process.env.KMS_KEY_ID,
+  keyVersion: process.env.KMS_KEY_VERSION,
+};
+
+const signer = new TestGcpKmsSigner(kmsCredentials, provider, process.env.TESTER_PRIVATE_KEY);
+
+const usdcAddress = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
+const usdcABI = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "approve",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "transfer",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+const usdcContract = new Contract(usdcAddress, usdcABI, provider);
+
+async function main(toUserAddress: string, amount: number) {
+  const fromUserAddress = await signer.getAddress();
+  console.log(`fromUserAddress: ${fromUserAddress}, toUserAddress: ${toUserAddress}`);
+  const fromUserBalance1 = await usdcContract.balanceOf(fromUserAddress);
+  const toUserBalance1 = await usdcContract.balanceOf(toUserAddress);
+  console.log(`fromUserBalance1: ${fromUserBalance1}, toUserBalance1: ${toUserBalance1}`);
+  const actualAmount = new BN(amount).mul(new BN(10).pow(new BN(6)));
+  console.log(`actualAmount: ${actualAmount}`);
+  const tx = await usdcContract.transfer.populateTransaction(toUserAddress, actualAmount.toNumber());
+  console.log(`tx: ${JSON.stringify(tx)}`);
+  const network = await provider.getNetwork();
+  const chainId = network.chainId.toString();
+  console.log(`chainId: ${chainId}`);
+  tx.chainId = network.chainId;
+  tx.gasLimit = await usdcContract.transfer.estimateGas(toUserAddress, actualAmount.toNumber());
+  tx.nonce = await provider.getTransactionCount(fromUserAddress);
+  const block = await provider.getBlock("latest");
+  tx.maxFeePerGas = block.baseFeePerGas;
+  const signedTx = await signer.signTransaction(tx);
+  console.log(`signedTx: ${signedTx}`);
+  const receipt = await provider.broadcastTransaction(signedTx);
+  console.log(`receipt: ${receipt.hash}`);
+  const fromUserBalance2 = await usdcContract.balanceOf(fromUserAddress);
+  const toUserBalance2 = await usdcContract.balanceOf(toUserAddress);
+  console.log(`fromUserBalance1: ${fromUserBalance1}, fromUserBalance2: ${fromUserBalance2}`);
+  console.log(`toUserBalance1: ${toUserBalance1}, toUserBalance2: ${toUserBalance2}`);
+}
+
+main("0x70997970C51812dc3A010C7d01b50e0d17dc79C8", 100)
+  .then(() => {
+    console.log("done");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
---
+++ src/signer-test.ts
@@ -0,0 +1,161 @@
+import configs from "dotenv";
+import BN from "bn.js";
+import {
+  MessageTypes,
+  SignTypedDataVersion,
+  TypedDataV1,
+  TypedMessage,
+  typedSignatureHash,
+  TypedDataUtils,
+} from "@metamask/eth-sig-util";
+
+import {
+  Provider,
+  AbstractSigner,
+  Transaction,
+  Signature,
+  TransactionRequest,
+  TransactionLike,
+  defineProperties,
+  getBytes,
+  hashMessage,
+  resolveProperties,
+  keccak256,
+  Wallet,
+  SigningKey,
+} from "ethers";
+import { bufferToHex } from "ethereumjs-util";
+import { getPublicKey, getEthereumAddress, requestKmsSignature, determineCorrectV } from "./util/gcp-kms-utils";
+import { validateVersion } from "./util/signature-utils";
+import { wallet, signingKey } from "./util/test-helper";
+
+configs.config();
+
+type Deferrable<T> = {
+  [K in keyof T]: T[K] | Promise<T[K]>;
+};
+
+export const TypedDataVersion = SignTypedDataVersion;
+
+export interface GcpKmsSignerCredentials {
+  projectId: string;
+  locationId: string;
+  keyRingId: string;
+  keyId: string;
+  keyVersion: string;
+}
+
+export class TestGcpKmsSigner extends AbstractSigner {
+  kmsCredentials: GcpKmsSignerCredentials;
+
+  ethereumAddress: string;
+
+  privateKey?: string;
+
+  constructor(kmsCredentials: GcpKmsSignerCredentials, provider?: Provider, privateKey?: string) {
+    super(provider);
+    defineProperties<TestGcpKmsSigner>(this, { provider: provider || null });
+    defineProperties<TestGcpKmsSigner>(this, { kmsCredentials });
+    this.privateKey = privateKey;
+  }
+
+  async getAddress(): Promise<string> {
+    if (this.ethereumAddress === undefined) {
+      if (this.privateKey) {
+        this.ethereumAddress = new Wallet(this.privateKey).address;
+      } else {
+        const key = await getPublicKey(this.kmsCredentials);
+        this.ethereumAddress = getEthereumAddress(key);
+      }
+    }
+    return Promise.resolve(this.ethereumAddress);
+  }
+
+  async _signDigest(digestString: string): Promise<string> {
+    const digestBuffer = Buffer.from(getBytes(digestString));
+    let sig = { r: new BN(0), s: new BN(0) };
+    if (this.privateKey) {
+      const { r, s, v } = new SigningKey(this.privateKey).sign(digestBuffer);
+      console.log(`sig.r: ${r}`);
+      console.log(`sig.s: ${s}`);
+      console.log(`sig.v: ${v}`);
+      sig.r = new BN(getBytes(r, "hex"));
+      sig.s = new BN(getBytes(s, "hex"));
+    } else {
+      sig = await requestKmsSignature(digestBuffer, this.kmsCredentials);
+    }
+    const ethAddr = await this.getAddress();
+    const { v } = determineCorrectV(digestBuffer, sig.r, sig.s, ethAddr);
+    console.log(`v: ${v}`);
+    return Signature.from({
+      v,
+      r: `0x${sig.r.toString("hex")}`,
+      s: `0x${sig.s.toString("hex")}`,
+    }).serialized;
+  }
+
+  async signMessage(message: string | Uint8Array): Promise<string> {
+    return this._signDigest(hashMessage(message));
+  }
+
+  /**
+   * Original implementation takes into account the private key, but here we use the private
+   * key from the GCP KMS, so we don't need to provide the PK as signature option.
+   * Source code: https://github.com/MetaMask/eth-sig-util/blob/main/src/sign-typed-data.ts#L510
+   * .
+   * Sign typed data according to EIP-712. The signing differs based upon the `version`.
+   *
+   * V1 is based upon [an early version of EIP-712](https://github.com/ethereum/EIPs/pull/712/commits/21abe254fe0452d8583d5b132b1d7be87c0439ca)
+   * that lacked some later security improvements, and should generally be neglected in favor of
+   * later versions.
+   *
+   * V3 is based on [EIP-712](https://eips.ethereum.org/EIPS/eip-712), except that arrays and
+   * recursive data structures are not supported.
+   *
+   * V4 is based on [EIP-712](https://eips.ethereum.org/EIPS/eip-712), and includes full support of
+   * arrays and recursive data structures.
+   *
+   * @param options - The signing options.
+   * @param options.data - The typed data to sign.
+   * @param options.version - The signing version to use.
+   * @returns The '0x'-prefixed hex encoded signature.
+   */
+  async signTypedData<V extends SignTypedDataVersion, T extends MessageTypes>({
+    data,
+    version,
+  }: {
+    data: V extends "V1" ? TypedDataV1 : TypedMessage<T>;
+    version: V;
+  }): Promise<string> {
+    validateVersion(version);
+
+    if (data === null || data === undefined) {
+      throw new Error("Missing data parameter");
+    }
+
+    let messageSignature: Promise<string>;
+    if (version === SignTypedDataVersion.V1) {
+      messageSignature = this._signDigest(typedSignatureHash(data as TypedDataV1));
+    } else {
+      const eip712Hash: Buffer = TypedDataUtils.eip712Hash(
+        data as TypedMessage<T>,
+        version as SignTypedDataVersion.V3 | SignTypedDataVersion.V4
+      );
+      messageSignature = this._signDigest(bufferToHex(eip712Hash));
+    }
+    return messageSignature;
+  }
+
+  async signTransaction(transaction: Deferrable<TransactionRequest>): Promise<string> {
+    const unsignedTx = await resolveProperties(transaction);
+    const trans: Transaction = Transaction.from(<TransactionLike>unsignedTx);
+    const serializedTx = trans.unsignedSerialized;
+    const transactionSignature = await this._signDigest(keccak256(serializedTx));
+    trans.signature = Signature.from(transactionSignature);
+    return trans.serialized;
+  }
+
+  connect(provider: Provider): TestGcpKmsSigner {
+    return new TestGcpKmsSigner(this.kmsCredentials, provider);
+  }
+}
